8ed6001f78d41ced36d19e091061c293595f0104
phajy <andy.young@bristol.ac.uk>
2024-12-03 13:42:21 +0000

    Dec 03 Part B

---
+++ dec03/03-b.jl
@@ -0,0 +1,24 @@
+include("../scripts/common.jl")
+
+# filename = "dec03/03-example-b.txt"
+filename = "dec03/03-input.txt"
+
+instruction_lines = aoc_read_lines(filename)
+
+total = 0
+factor = 1
+for instruction in instruction_lines
+    for m in eachmatch(r"(mul\(([0-9]|[0-9][0-9]|[0-9][0-9][0-9]),([0-9]|[0-9][0-9]|[0-9][0-9][0-9])\)|don\'t\(\)|do\(\))", instruction)
+        if m.match == "do()"
+            factor = 1
+        end
+        if m.match == "don't()"
+            factor = 0
+        end
+        if m.match[1:3] == "mul"
+            total = total + factor * parse(Int, m[2]) * parse(Int, m[3])
+        end
+    end
+end
+
+println("Total = ", total)
